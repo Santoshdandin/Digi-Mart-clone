f31462d02498c90540f3fd102f8daa19dea974bc
Santoshdandin <santusb427@gmail.com>
2023-01-15 13:43:52 +0530

    created reliance digital clone project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Text} from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Text as="h" fontSize="100px" >Reliance digital</Text>
+      
+      
+    </div>
+  );
+}
+
+export default App;
