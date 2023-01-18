de0efede635f18b277495d8ffee771abe5158c44
sumeetmore1997 <101966263+sumeetmore1997@users.noreply.github.com>
2023-01-18 23:36:18 +0530

    basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import { Text} from '@chakra-ui/react'
+import Cart from './Components/Cart';
 
 function App() {
   return (
     <div className="App">
      
       <Text as="h" fontSize="100px" >Reliance digital</Text>
-      
+      <Cart/>
       
     </div>
   );
---
+++ frontend/src/Components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box} from "@chakra-ui/react"
+
+const Cart = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default Cart
