056ea3543923a09b4ab8028c49e99ba8d8e10d6d
Ankur9900 <arya.ankur99@gmail.com>
2023-01-19 11:35:15 +0530

    day-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Footer from './Components/Pages/Footer';
+import Navbar from './Components/navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
 <Footer/>
      
     </div>
